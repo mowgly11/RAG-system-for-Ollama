e05d92155dcfda6d4601065759e0ed5db30e6e53
John <83482871+mowgly11@users.noreply.github.com>
2026-08-29 12:58:47 +0100

    feat: removed TEMPERATURE and added TOR_PROXY_URL
    moved the temperature property to config.json and added tor proxy url to support searching through the tor network for more uncensorship

--- env.ts
+++ env.ts
@@ -6,7 +6,7 @@ const envSchema = z.object({
     QUERY_MODEL: z.string().default("llama3.2:1b"),
     OLLAMA_HOST: z.url().default("http://127.0.0.1:11434"),
     VECTOR_STORE_COLLECTION_NAME: z.string().default('rag_store'),
-    TEMPERATURE: z.coerce.number().min(0).max(1).default(0.7)
+    TOR_PROXY_URL: z.string().default("socks5://127.0.0.1:9050")
 });
 
 const env = envSchema.parse(process.env);
